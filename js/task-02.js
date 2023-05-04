24bc05b4d03ceaa45de858f9d31c991c113396cd
SergiyZatsarinniy <Zatsarinniys@gmail.com>
2023-05-04 23:23:06 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+const fragment = document.createDocumentFragment();
+ingredients.forEach((ingredient) => {
+  const listEl = document.createElement('li');
+  listEl.textContent = ingredient;
+  listEl.classList.add('item');
+  fragment.appendChild(listEl);
+});
+ingredientsList.appendChild(fragment);
