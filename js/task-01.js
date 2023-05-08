c551a0df9d7e269c9534fab91407c80df7c7abfb
SergiyZatsarinniy <Zatsarinniys@gmail.com>
2023-05-08 23:11:53 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,6 @@
-const categoriesList = document.querySelector('#categories');
-const categoriesItems = categoriesList.querySelectorAll('.item');
-console.log(`Number of categories: ${categoriesItems.length}`);
+const categoriesItems = document.querySelectorAll('.item');
+console.log('Number of categories:', categoriesItems.length);
 categoriesItems.forEach((item) => {
-  const categoryName = item.querySelector('h2').textContent;
-  const categoryElements = item.querySelectorAll('li').length;
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryElements}`);
+  console.log('Category:', item.firstElementChild.textContent);
+  console.log('Elements:', item.lastElementChild.children.length);
 });
