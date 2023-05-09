a2031ddc8669bd2aac9e73f6ae6e4b9e8363cf3a
SergiyZatsarinniy <Zatsarinniys@gmail.com>
2023-05-09 16:11:37 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginFormRef = document.querySelector('.login-form');
 
+loginFormRef.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const email = loginFormRef.elements.email.value;
+  const password = loginFormRef.elements.password.value;
+
+  if (email === '' || password === '') {
+    return alert('Всі поля повинні бути заповнені');
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+  loginFormRef.reset();
+});
